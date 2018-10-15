021673428dfc9e7299570f3c4a1b20c6caa68a4e
levantuy <levantuy.it@gmail.com>
2018-10-16 00:46:16 +0700

    errors

--- SplitExcelApp/Form1.cs
+++ SplitExcelApp/Form1.cs
@@ -67,32 +67,54 @@ namespace SplitExcelApp
 
             var distinctRows = (from DataRow dRow in dataTable.Rows
                                 select new { ClassName = dRow["Class"].ToString(), ClassDay = dRow["Day"].ToString() }).Distinct().OrderBy(x => x.ClassName);
-
+            var sheetErrors = workbookNew.Worksheets.Add("Errors");
+            workbook.Worksheets[sheetErrors.Name].CopyFrom(workbook.Worksheets["tempSheet"]);
+            var errorCount = 5;
             foreach (var info in distinctRows)
             {
                 var sheet = workbookNew.Worksheets.Add();                
                 sheet.Name = info.ClassName + " " + ConvertToEnglish(info.ClassDay);
                 workbook.Worksheets[sheet.Name].CopyFrom(workbook.Worksheets["tempSheet"]);
                 workbook.Worksheets[sheet.Name].Cells["C2"].Value = "LỚP: " + info.ClassName;
-                var count = 5;
+                var count = 5;                
                 foreach (DataRow row in dataTableSort)
                 {
                     if (row["Class"].ToString() == info.ClassName && row["Day"].ToString() == info.ClassDay)
                     {
-                        count++;
-                        sheet.Rows.Insert(count);
-                        sheet.Rows[count].CopyFrom(sheet.Rows[count + 1]);
-                        sheet.Rows[count][1].Value = (count - 5).ToString();
-                        sheet.Rows[count][2].Value = row[0].ToString();
-                        sheet.Rows[count][3].Value = row[1].ToString().Length > 0 ? Convert.ToDouble(row[1]) : 0;
-                        sheet.Rows[count][4].Value = ConvertDateToString(row[2].ToString());
-                        sheet.Rows[count][5].Value = row[3].ToString();
-                        sheet.Rows[count][6].Value = row[4].ToString();
-                        sheet.Cells["Q" + (count + 1).ToString()].Value = row[7].ToString();
+                        try
+                        {
+                            count++;
+                            sheet.Rows.Insert(count);
+                            sheet.Rows[count].CopyFrom(sheet.Rows[count + 1], PasteSpecial.All);
+                            sheet.Rows[count][1].Value = (count - 5).ToString();
+                            sheet.Rows[count][2].Value = row[0].ToString();
+                            sheet.Rows[count][3].Value = row[1].ToString().Length > 0 ? Convert.ToDouble(row[1]) : 0;
+                            sheet.Rows[count][4].Value = row[2].ToString().Length > 6 ? Convert.ToDateTime(row[2]).ToString("dd/MM/yyyy") : row[2].ToString();
+                            sheet.Rows[count][5].Value = row[3].ToString();
+                            sheet.Rows[count][6].Value = row[4].ToString().Length > 0 ? Convert.ToDouble(row[4]) : 0;
+                            sheet.Cells["Q" + (count + 1).ToString()].Value = row[7].ToString();
+                        }
+                        catch(Exception ex)
+                        {
+                            sheet.Rows.Remove(count);
+                            count--;
+                            errorCount++;
+                            sheetErrors.Rows.Insert(errorCount);
+                            sheetErrors.Rows[errorCount].CopyFrom(sheetErrors.Rows[errorCount + 1], PasteSpecial.All);
+                            sheetErrors.Rows[errorCount][1].Value = (errorCount - 5).ToString();
+                            sheetErrors.Rows[errorCount][2].Value = row[0].ToString();
+                            sheetErrors.Rows[errorCount][3].Value = row[1].ToString();
+                            sheetErrors.Rows[errorCount][4].Value = row[2].ToString();
+                            sheetErrors.Rows[errorCount][5].Value = row[3].ToString();
+                            sheetErrors.Rows[errorCount][6].Value = row[4].ToString();
+                            sheetErrors.Cells["Q" + (errorCount + 1).ToString()].Value = row[7].ToString();
+                            sheetErrors.Cells["R" + (errorCount + 1).ToString()].Value = ex.Message;
+                        }                        
                     }
                 }
-                sheet.Rows.Remove(count + 1);
+                //sheet.Rows.Remove(count + 1);
             }
+            workbookNew.Worksheets.RemoveAt(0);
         }
 
         public string ConvertToEnglish(string vietnamese)
